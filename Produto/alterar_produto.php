8eed09824f0a113c3223adaaf04c9ab356337156
henryy0 <you@example.com>
2024-02-27 16:07:55 -0300

    Versão Beta

--- Produto/alterar_produto.php
+++ Produto/alterar_produto.php
@@ -1,36 +1,32 @@
 <?php
-
-if (isset($_POST['Cadastrar'])) {
+if (isset($_POST['Alterar'])) {
     try {
-        // Inclua o arquivo de conexão
-        include_once('../conexao.php');
-
-        // Preparar a consulta SQL
+        include_once('conexao.php');
         $sql = $conn->prepare(
-            'INSERT INTO produto
-                (nome_Produto, data_Produto, qtde_Produto, Vcusto_Produto, Vvenda_Produto, obs_Produto, status_Produto)
-                VALUES
-                (:nome_Produto, NOW(), :qtde_Produto, :Vcusto_Produto, :Vvenda_Produto, :obs_Produto, :status_Produto)'
+            'UPDATE produto SET
+                nome_Produto = :nome_produto,
+                qtde_Produto = :qtde_produto,
+                Vcusto_Produto = :vcusto_produto,
+                Vvenda_Produto = :vvenda_produto,
+                obs_Produto = :obs_produto,
+                status_Produto = :status_produto
+            WHERE ID_Produto = :id_produto'
         );
-
-        // Executar a consulta
         $sql->execute(array(
-            ':nome_Produto' => $_POST['NomeProduto'],
-            ':qtde_Produto' => $_POST['Quantidade'],
-            ':Vcusto_Produto' => $_POST['ValorCusto'],
-            ':Vvenda_Produto' => $_POST['ValorVenda'],
-            ':obs_Produto' => $_POST['Observacao'],
-            ':status_Produto' => $_POST['Status'],
+            ':id_produto' => $_POST['id_produto'],
+            ':nome_produto' => $_POST['Nome'],
+            ':qtde_produto' => $_POST['Quantidade'],
+            ':vcusto_produto' => $_POST['Vcusto'],
+            ':vvenda_produto' => $_POST['Vvenda'],
+            ':obs_produto' => $_POST['Observacao'],
+            ':status_produto' => $_POST['status_produto']
         ));
-
-        // Verificar se o cadastro foi realizado com sucesso
         if ($sql->rowCount() > 0) {
-            echo '<p> Cadastro realizado com sucesso. </p>';
-            echo '<p> ID Gerado: ' . $conn->lastInsertId() . '</p>';
+            echo '<p> Produto alterado com sucesso</p>' . $sql->rowCount();
+            echo '<p> ID Alteração </p>' . $_POST['id_produto'] . '</p>';
         }
     } catch (PDOException $error) {
         echo $error->getMessage();
     }
 }
-
 ?>
